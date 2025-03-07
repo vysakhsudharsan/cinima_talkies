a183b46d77524cad9ce541639ebd1286d21d1ae0
vysakh-sudarsan <vysakh.s@vandalayglobal.com>
2025-03-07 17:14:49 +0530

    Add vite.config.js for configuration

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/cinima_talkies/',  // Add this line to set the base path for GitHub Pages
 })
